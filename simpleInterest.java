4453c3f6e71a4c9cdb76c8adbd4b63c84584a3c8
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-06-14 20:49:02 -0500

    Initial commit DSA Practice Problems

---
+++ simpleInterest.java
@@ -0,0 +1,13 @@
+public class simpleInterest {
+
+    public static int calculateInterest(int principle , int time , int roi){
+        int Interest = (principle*time*roi)/100 ;
+        return Interest;
+    }
+    public static void main(String[] args){
+        System.out.println("Simple Interest");
+        int result = calculateInterest(50000, 1, 2);
+        System.out.println("The calcualted simple interest is "+result);
+    }
+    
+}
---
+++ sumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+public class sumOfTwoNumbers{
+
+public static int sum(int a , int b){
+    int sum = a+b;
+    return sum;
+}
+
+    public static void main(String[] args){
+        System.out.println("Test this code ");
+        int result = sum(8,9);
+        System.out.println("Sum of Two given numbers is "+result);
+
+    }
+}
