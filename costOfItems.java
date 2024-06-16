073c98d50d3a630f59921b31f6a8a8c7fbac1b5c
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-06-16 14:48:45 -0500

    Practice Cost of Items (bug resolved ) based on user input java,It was nothing related to code or logic , its completely dependent on VS code execution patterns , when I run it using run button everything is smooth ,but when we try to do it through terminal ,it is behaving differently

--- costOfItems.java
+++ costOfItems.java
@@ -4,20 +4,21 @@ public class costOfItems {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the cost of pencil ");
-        System.out.flush();
         float costOfPencil = scan.nextFloat();
-        System.out.println("Cost of pencil is: " + costOfPencil);
+        System.out.println("The entered value for a pencil is " + costOfPencil);
         System.out.println("Enter the cost of pen");
         float costOfPen = scan.nextFloat();
+        System.out.println("The entered value for a pen is " + costOfPen);
         System.out.println("Enter the cost of eraser");
         float costOfEraser = scan.nextFloat();
+        System.out.println("The entered value for a eraser is " + costOfEraser);
         float total = (costOfPencil + costOfPen + costOfEraser);
-        float gstApplied = (18F / 100);
-        float calculatedGST = total * gstApplied;
+        float gstApplied = total * 0.18F;
+        float calculatedGST = total + gstApplied;
         // System.out.printf("The GST on the given bill is %.2f%n" + gst);
-        System.out.println("The GST on the given bill is" +calculatedGST);
-
-        //scan.close();
+        System.out.println("The GST on the given bill is" + calculatedGST);
+        System.out.println("The GST on the total amount is " + gstApplied);
+        scan.close();
     }
 
 }
