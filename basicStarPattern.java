53415f089c73ae24897e34b9057d6683277a35b7
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-06-19 06:54:11 -0500

    basic star pattern based on user input

--- basicStarPattern.java
+++ basicStarPattern.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class basicStarPattern {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        for(int i=1;i<=number;i++){ //outer loop
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
     
 }
