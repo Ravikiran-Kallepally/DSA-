42c851b8aa02735e461ae2701b811f135a6aa181
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-06-19 08:14:04 -0500

    average of 3 numbers method java

---
+++ averageOfThreeNumbers.java
@@ -0,0 +1,13 @@
+public class averageOfThreeNumbers {
+    public static void average(int a, int b, int c) {
+        int result = (a + b + c) / 3;
+        System.out.println("The average of given numbers is " + result);
+
+    }
+
+    public static void main(String[] args) {
+        average(22, 32, 42);
+
+    }
+
+}
