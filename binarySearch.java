fe0f4ec1f3dae9739ba77b9ab5be20b3d5aea3d1
Ravikiran Kallepally <ravikirankallepally@gmail.com>
2024-06-22 08:34:23 -0500

    binarySearch starter code

---
+++ binarySearch.java
@@ -0,0 +1,23 @@
+public class binarySearch {
+
+    public static int search(int[]a ,int key){
+        int start =a[0];
+        int last = a.length-1;
+        while(start<=last){
+            int mid = (start+last )/2;
+            if(mid==key){
+                System.out.println("Key Found ");
+            }else if(mid>key){
+
+            }else if(mid<key){
+            
+            }
+        }
+
+    }
+    public static void main(String[] args){
+
+
+    }
+    
+}
